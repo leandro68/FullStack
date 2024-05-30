bc8825fd7b0871b20daf9d92892892f92d3408f8
Lean <leandrorodriguez68@gmail.com>
2024-05-30 15:41:58 -0300

    Exercise 1.9 added

---
+++ part1/ex1.9 Unicafe part4/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const Title = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Statistic = ({ good, neutral, bad, all }) => {
+  if (all === 0) {
+    return <p>No feedback given</p>;
+  } else {
+    return (
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {(good - bad) / all}</p>
+        <p>positive {good / all}%</p>
+      </div>
+    );
+  }
+};
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  };
+
+  return (
+    <div>
+      <Title text="give feedback" />
+      <Button text="good" handleClick={handleGood} />
+      <Button text="neutral" handleClick={handleNeutral} />
+      <Button text="bad" handleClick={handleBad} />
+      <Title text="statistics" />
+      <Statistic good={good} neutral={neutral} bad={bad} all={all} />
+    </div>
+  );
+};
+
+export default App;
