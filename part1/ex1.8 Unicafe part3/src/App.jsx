410cd4759da8905b76df9c2398cd8992317f3951
Lean <leandrorodriguez68@gmail.com>
2024-05-30 14:43:55 -0300

    Exercise 1.8 added

---
+++ part1/ex1.8 Unicafe part3/src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+
+const Title = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Total = ({ text, state, symbol }) => {
+  return (
+    <div>
+      <p>
+        {text} {state} {symbol}
+      </p>
+    </div>
+  );
+};
+
+const Statistics = ({ text, state, symbol }) => {
+  return (
+    <div>
+      <p>
+        {text} {state} {symbol}
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positivePerc, setPositivePerc] = useState(0);
+  const pointsGood = 1;
+  const pointsBad = -1;
+
+  const handleGood = () => {
+    const total = all + 1;
+    const goodTotal = good + 1;
+    setGood(good + 1);
+    setAll(all + 1);
+    setAverage((goodTotal * pointsGood + bad * pointsBad) / total);
+    setPositivePerc(goodTotal / total);
+  };
+
+  const handleNeutral = () => {
+    const total = all + 1;
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+    setAverage((good * pointsGood + bad * pointsBad) / total);
+    setPositivePerc(good / total);
+  };
+
+  const handleBad = () => {
+    const total = all + 1;
+    const badTotal = bad + 1;
+    setBad(bad + 1);
+    setAll(all + 1);
+    setAverage((good * pointsGood + badTotal * pointsBad) / total);
+    setPositivePerc(good / total);
+  };
+
+  return (
+    <div>
+      <Title text="give feedback" />
+      <Button text="good" handleClick={handleGood} />
+      <Button text="neutral" handleClick={handleNeutral} />
+      <Button text="bad" handleClick={handleBad} />
+      <Title text="statistics" />
+      <Total text="good" state={good} />
+      <Total text="neutral" state={neutral} />
+      <Total text="bad" state={bad} />
+      <Statistics text="all" state={all} />
+      <Statistics text="average" state={average} />
+      <Statistics text="positive" state={positivePerc} symbol="%" />
+    </div>
+  );
+};
+
+export default App;
