2075817b798aa75e7ebad92eeac32abc662ad550
Lean <leandrorodriguez68@gmail.com>
2024-05-30 17:42:15 -0300

    Exercise 1.11 added

---
+++ part1/ex1.11 Unicafe part6/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+const Title = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Statistic = ({ good, neutral, bad, all }) => {
+  if (all === 0) {
+    return <p>No feedback given</p>;
+  } else {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+          <td>bad</td>
+          <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{all}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{(good - bad) / all}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{good / all}%</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+};
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  };
+
+  return (
+    <div>
+      <Title text="give feedback" />
+      <Button text="good" handleClick={handleGood} />
+      <Button text="neutral" handleClick={handleNeutral} />
+      <Button text="bad" handleClick={handleBad} />
+      <Title text="statistics" />
+      <Statistic good={good} neutral={neutral} bad={bad} all={all} />
+    </div>
+  );
+};
+
+export default App;
