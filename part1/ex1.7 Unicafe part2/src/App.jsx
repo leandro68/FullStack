6bf6189d3472f03af0a1e2369f41143a849774f9
Lean <leandrorodriguez68@gmail.com>
2024-05-30 15:26:08 -0300

    Exercise 1.7 improved

--- part1/ex1.7 Unicafe part2/src/App.jsx
+++ part1/ex1.7 Unicafe part2/src/App.jsx
@@ -1,24 +1,25 @@
 import { useState } from 'react'
 
-  const Title = ({text}) => {
-    return (
-      <h1>{text}</h1>
-    )
-  }
+const Title = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
 
-  const Button = ({text, handleClick}) => {
-    return (
-      <button onClick={handleClick}>{text}</button>
-    )
-  }
+const Button = ({text, handleClick}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
 
-  const Total =({text, state, symbol}) => {
-    return (
-        <div>
-          <p>{text} {state} {symbol}</p>
-        </div>
-    )
-  }
+const Total =({text, total, symbol}) => {
+  return (
+    <div>
+      <p>{text} {total}{symbol}</p>
+    </div>
+    
+  )
+}
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
@@ -26,36 +27,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positivePerc, setPositivePerc] = useState(0)
-  const pointsGood = 1
-  const pointsBad = -1
-  
+
   const handleGood = () => {
-      const total = all + 1
-      const goodTotal = good + 1
-      setGood( good + 1 )
-      setAll( all + 1 )
-      setAverage( (goodTotal * pointsGood + bad * pointsBad) / total )
-      setPositivePerc( goodTotal / total )
+      setGood(good+1)
+      setAll(all+1)
   }
 
   const handleNeutral = () => {
-      const total = all + 1
-      setNeutral( neutral+1 )
-      setAll( all + 1 )
-      setAverage( ((good * pointsGood) + (bad * pointsBad)) / total )
-      setPositivePerc( good / total )
+      setNeutral(neutral+1)
+      setAll(all+1)
   }
 
   const handleBad = () => {
-      const total = all + 1
-      const badTotal = bad + 1
-      setBad( bad+1 )
-      setAll( all + 1 )
-      setAverage( ( good * pointsGood + badTotal * pointsBad ) / total )
-      setPositivePerc( good / total )
+      setBad(bad+1)
+      setAll(all+1)
   }
+
 
   return (
     <div>
@@ -64,12 +51,12 @@ const App = () => {
       <Button text="neutral" handleClick={handleNeutral}/>
       <Button text="bad" handleClick={handleBad}/>
       <Title text="statistics" />
-      <Total text="good" state={good}/>
-      <Total text="neutral" state={neutral}/>
-      <Total text="bad" state={bad}/>
-      <Total text="all" state={all}/>
-      <Total text="average" state={average}/>
-      <Total text="positive" state={positivePerc} symbol="%"/>
+      <Total text="good" total={good}/>
+      <Total text="neutral" total={neutral}/>
+      <Total text="bad" total={bad}/>
+      <Total text="all" total={all}/>
+      <Total text="average" total={(good - bad) / all}/>
+      <Total text="positive" total={good / all} symbol="%"/>
     </div>
   )
 }
