3b293a98b52f13cd5920b39286ab43326d82b45d
Lean <leandrorodriguez68@gmail.com>
2024-05-31 14:35:45 -0300

    Exercise 2.3 added

---
+++ part2/ex2.3/src/components/Course.jsx
@@ -0,0 +1,37 @@
+const Header = ({ name }) => <h1>{name}</h1>
+
+const Total = ({ parts }) => {
+  let total = parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)
+  return (
+    <p>Total of {total} exercises</p>
+  )
+  
+}
+
+const Part = ({part}) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+       {parts.map((part => <Part key={part.id} part={part} />))}
+    </> 
+  )
+}
+
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+    
+  )
+}
+
+export default Course;
