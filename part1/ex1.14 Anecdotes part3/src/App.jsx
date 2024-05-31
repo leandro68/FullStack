fff7cbe69c47adb1ba2db472b5347b542e62cf37
Lean <leandrorodriguez68@gmail.com>
2024-05-31 09:35:02 -0300

    exercise 1.14 added

---
+++ part1/ex1.14 Anecdotes part3/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+
+const Title = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Anecdote = ({text}) => {
+  return (
+    <p>{text}</p>
+  )
+}
+
+const Button = ({text, handleClick}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const VotesPerAnecdote = ({anecdoteVotes}) => {
+  return (
+    <p>Has {anecdoteVotes} votes</p>
+  )
+}
+
+
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  //array with votes per anecdote
+  const votes = Array.from({ length: anecdotes.length }, () => 0)
+ 
+  //save anecdote index selected
+  const [selected, setSelected] = useState(0)
+  const [votesList, setVote] = useState(votes)
+  const [mostVoted, setMostVoted] = useState(0)
+
+  //random generator
+  function newRandom(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  //set new vote in the array of votes state
+  //and set most voted
+  const handleVote = () => {
+    const newVotesList = [].concat(votesList)
+    newVotesList[selected] += 1
+    setVote(newVotesList)
+    setMostVoted(newVotesList.indexOf(Math.max(...newVotesList)))
+  }
+
+  //set new anecdote index selected state
+  const handleSelected = () => {
+    const indexSelected = newRandom(anecdotes.length)
+    setSelected(indexSelected)
+  }
+
+  return (
+    <div>
+      <Title text="Anecdote of the day" />
+      <Anecdote text={anecdotes[selected]} />
+      <VotesPerAnecdote anecdoteVotes={votesList[selected]}/>
+      <Button text="vote" handleClick={handleVote} />
+      <Button text="next Anecodote" handleClick={handleSelected} />
+      <Title text="Anecdote with most votes" />
+      <Anecdote text={anecdotes[mostVoted]} />
+    </div>
+  )
+}
+
+export default App
