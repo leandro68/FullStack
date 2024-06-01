16293c747fd6ed88cfb2934eb941de80c4cb9d66
Lean <leandrorodriguez68@gmail.com>
2024-06-01 14:20:18 -0300

    exercise 2.7 added

---
+++ part2/ex2.7 Phonebook part 2/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newPerson, setNewPerson] = useState('')
+
+  const addPerson = (event) => {
+        event.preventDefault()
+        const person = {
+          id : persons.length,
+          name: newPerson
+        }
+        if (persons.some(personInPersons => personInPersons.name === person.name)){
+          alert("Ese nombre ya está agendado")
+        }
+        else {
+          setPersons(persons.concat(person))
+        }
+  }
+
+  const handleNameChange = (event) => {
+    setNewPerson(event.target.value)
+  }
+
+  const PersonLine = () => {
+    return (
+      <p>{persons.map(person => <li key={person.id}>{person.name}</li>)}</p>
+    )
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newPerson} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        <PersonLine />
+      </ul>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
