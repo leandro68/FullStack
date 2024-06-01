78a63bf5eb64a2ee442136621abfe0042793fa48
Lean <leandrorodriguez68@gmail.com>
2024-06-01 14:04:26 -0300

    exercise 2.6 added

---
+++ part2/ex2.6 Phonebook part 1/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newPerson, setNewPerson] = useState('')
+
+  const addPerson = (event) => {
+        event.preventDefault()
+        const person = {
+          id : persons.length,
+          name: newPerson
+        }
+        setPersons(persons.concat(person))
+  }
+
+  const handleNameChange = (event) => {
+    setNewPerson(event.target.value)
+  }
+
+  const PersonLine = () => {
+    return (
+      <p>{persons.map(person => <li key={person.id}>{person.name}</li>)}</p>
+    )
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newPerson} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        <PersonLine />
+      </ul>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
