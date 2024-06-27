f9732b196446adb2a406e58d18342af1bd7ee78c
Lean <leandrorodriguez68@gmail.com>
2024-06-27 16:34:59 -0300

    exercise 4.8 added

---
+++ part4/Ex 4.8/tests/test_helper.js
@@ -0,0 +1,33 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Blog01',
+    author: 'Author01',
+    url: 'url1.com',
+    likes: 3
+  },
+  {
+    title: 'Blog02',
+    author: 'Author02',
+    url: 'url2.com',
+    likes: 12
+  }
+]
+
+const nonExistingId = async () => {
+  const blog = new Blog({ content: 'willremovethissoon' })
+  await blog.save()
+  await blog.deleteOne()
+
+  return blog._id.toString()
+}
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, nonExistingId, blogsInDb
+}
