c2d7f7c571e9a54e73004be7a72e2e82d0921f1a
Lean <leandrorodriguez68@gmail.com>
2024-07-01 14:21:19 -0300

    Exercise 4.18

---
+++ part4/Ex 4.18/controllers/users.js
@@ -0,0 +1,30 @@
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+const bcryptjs = require('bcryptjs')
+
+usersRouter.post('/', async (request, response) => {
+  if (request.body.password.length < 3) {
+    return response.status(400).json({ error: 'password must be at least 3 characters' })
+  } else {
+    const { username, name, password } = request.body
+    const saltRounds = 10
+    const passwordHash = await bcryptjs.hash(password, saltRounds)
+
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+  }
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs', { content: 1, important: 1 })
+  response.json(users)
+})
+
+module.exports = usersRouter
