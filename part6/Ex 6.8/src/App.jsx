a058a2eea846400abbd3011c1182d04b06c87df1
Lean <leandrorodriguez68@gmail.com>
2024-07-18 12:25:43 -0300

    Exercise 6.8

---
+++ part6/Ex 6.8/src/App.jsx
@@ -0,0 +1,16 @@
+import NewAnecdote from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+
+const App = () => {
+  
+  
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <AnecdoteList />
+      <NewAnecdote />
+    </div>
+  )
+}
+
+export default App
