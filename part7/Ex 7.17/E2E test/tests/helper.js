14e0c28b2876039877bce22ba463cfa62484ca35
Lean <leandrorodriguez68@gmail.com>
2024-08-09 11:09:36 -0300

    Exercise 7.17

---
+++ part7/Ex 7.17/E2E test/tests/helper.js
@@ -0,0 +1,17 @@
+const loginWith = async (page, username, password)  => {
+  await page.getByRole('button', { name: 'log in' }).click()
+  await page.getByTestId('username').fill(username)
+  await page.getByTestId('password').fill(password)
+  await page.getByRole('button', { name: 'login' }).click()
+}
+
+const createBlog = async (page, title, author, url) => {
+  await page.getByRole('button', { name: 'Add Blog' }).click()
+  await page.getByTestId('blogTitle').fill(title)
+  await page.getByTestId('blogAuthor').fill(author)
+  await page.getByTestId('blogUrl').fill(url)
+  await page.getByRole('button', { name: 'save' }).click()
+  await page.getByText(`${title}, ${author}`).waitFor()
+}
+
+export { loginWith, createBlog }
---
+++ part7/Ex 7.17/frontend/src/components/Logout.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable semi */
+/* prettier-ignore */
+import { useDispatch, useSelector } from 'react-redux'
+import { setUser } from '../reducers/userReducer'
+import { setNotification } from '../reducers/notificationReducer'
+import { useNavigate } from 'react-router-dom'
+
+const Logout = () => {
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.user)
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    console.log('logout')
+    window.localStorage.removeItem('loggedBlogappUser')
+    dispatch(setUser(null))
+    dispatch(setNotification('Logout successful'))
+    setTimeout(() => {
+      dispatch(setNotification(null))
+    }, 5000)
+    navigate('/')
+    //dispatch(setBlogs(blogs))
+  }
+
+  return (
+    <div className="logout-section">
+      <p>{user.name} logged in</p>
+      <button
+        onClick={() => {
+          handleLogout()
+        }}
+      >
+        logout
+      </button>
+    </div>
+  )
+}
+
+export default Logout
